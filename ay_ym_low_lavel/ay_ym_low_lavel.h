52d53348a9fe6b454ed43797af2de3233cdce3f4
Vadimatorik <deryabkinda@mail.ru>
2017-07-08 12:16:30 +0700

    Получин звук в обход усилителя. Требуется реализовать управление микшированием каналов.

--- ay_ym_low_lavel/ay_ym_low_lavel.h
+++ ay_ym_low_lavel/ay_ym_low_lavel.h
@@ -27,19 +27,7 @@ struct ay_ym_low_lavel_cfg_t {
                                                              * В таком случае следует передать указатель на байт, являющимся 4-м байтом сдвигового регистра.
                                                              * Массив для сдвиговых регистров должен быть создан заранее и, в идеале, быть глобальным.
                                                              */
-    const uint8_t                   priority_task;          /*
-                                                             * Приоритет задачи AY, которая будет выдавать данные из очереди в чип.
-                                                             * Вывод осуществляется параллельно в оба чипа (BC1 и BDIR выводы подключены параллельно).
-                                                             * В случае, если очередь в 1 чип не равна очерядям в какой-либо другой, то для соблюдения синхронной передачи
-                                                             * происходит запись 0 в регистр 16 (такого регистра нет, зато можно не беспокоиться о том, что это что-то испортит).
-                                                             * Пример: 2 чипа. AY (1) и YM (2). Подключены они на 0-й и 1-й байт расширителя портов соответственно.
-                                                             * Но в AY 4 байта, YM 1. Тогда при передачи первого пакета запись произойдет синхронно, а при записи
-                                                             * в AY 2-4 байта, параллельно в YM будет записано ранее упомянутое значение (по умолчанию 0, если во время воспроизведения
-                                                             * не было указано иного) в 16-й регистр (его нет).
-                                                             */
     USER_OS_STATIC_BIN_SEMAPHORE    *const semaphore_sec_out;// Этим симафором будем показывать, что прошла секунда воспроизведения. Опционально (можно NULL). Если не указан, просто игнорируется.
-    uint8_t                         ay_number;               // Колличество AY чипов (максимум 32).
-                                                             // Должен быть как минимум 1 AY.
     // Выводы управления AY должны быть указаны обязательно (включение всех AY - параллельное).
     // Выводы должны быть указано явно.
     const pin*                      const bdir;              // Выводы управления AY чипами (чипы включать в параллель).
@@ -50,7 +38,7 @@ struct ay_ym_low_lavel_cfg_t {
      * Здесь указатель на массив указателей на эти очереди.
      * Очередь должна содержать ay_low_out_data элементы!!!
      */
-    USER_OS_STATIC_QUEUE**          const p_queue_array;
+    USER_OS_STATIC_QUEUE*           const p_queue_array;
 };
 
 
@@ -61,21 +49,17 @@ struct ay_low_out_data {
 //int                    *tim_frequency_ay_fd;                // FD таймера, который генерирует необходимую частоту для генерации сигнала (~1.75 МГц по-умолчанию).
 //    int                    *tim_event_ay_fd;                    // FD таймера, вызывающего прерывания (лля вывода данных из очереди в AY/YM).
 
-
+/*
 void ay_clear_to_queue (int fd);                            // Очищаем AY через очередь.
 void ay_delay_clean (int fd);                                // Выходим после того, как очередь AY освободится.
 void ay_play_stait (int fd, uint8_t stait);                    // Включаем/выключаем AY. Причем сохраняя состояние регистров. Чтобы после запуска ничего не глючило.
 void ay_queue_add_element (int fd, void* data, uint8_t number_ay);            // Добавляет в очередь элемент.
 void ay_music_off (int fd);            // Отключаем воспроизведение (очищаем очередь, чистим AY, выключаем таймеры).
+*/
 
 /*
  * Очередь элементов для выдачи в AY.
  */
-//mutable uint8_t queue_buf[ ( sizeof( ay_queue_struct ) ) * QUEUE_LENGTH ] = { 0 };
-//mutable USER_OS_STATIC_QUEUE_STRUCT  q_st_buf             = USER_OS_STATIC_QUEUE_STRUCT_INIT_VALUE;
-//mutable USER_OS_STATIC_QUEUE         queue                = nullptr;
-
-
 struct ay_queue_struct {
     uint8_t     number_chip;
     uint8_t     reg;
@@ -84,13 +68,17 @@ struct ay_queue_struct {
 
 // Очередь должна быть как минимум 1 элемент (в идеале - по 16*2 b и более для каждого чипа).
 // Очердь общая для все чипов!
-template < uint8_t AY_NUMBER, uint8_t QUEUE_LENGTH >
+template < uint8_t AY_NUMBER, uint8_t TASK_PRIO, const ay_ym_low_lavel_cfg_t* const STRUCT_INIT >
 class ay_ym_low_lavel {
 public:
-    constexpr ay_ym_low_lavel ( const ay_ym_low_lavel_cfg_t* const cfg ) : cfg(cfg) {}
+
+
+    static const ay_ym_low_lavel< AY_NUMBER, TASK_PRIO, STRUCT_INIT >* instance ( void );
     void init ( void ) const;
 
 private:
+    constexpr ay_ym_low_lavel () : cfg(STRUCT_INIT) {}
+
     void out_reg    ( void ) const;
     void out_data   ( void ) const;
 
@@ -102,7 +90,7 @@ private:
 void hardware_clear ( void ) const;
 
     const ay_ym_low_lavel_cfg_t* const cfg;
-void task () const;
+    static void task ( void* p_this );
     /*
      * Этим симафором будем показывать, что пора передать следущую порцию данных.
      * Мы ждем его в задаче ay_queue_out_task и отдаем в ay_timer_handler.
@@ -119,6 +107,8 @@ void task () const;
     /*
      * Далее все сделано так, чтобы можно было поддерживать до 32 AY/YM чипов.
      */
-    mutable uint8_t     r7_reg[QUEUE_LENGTH] = { 0 };     // Текущее состояние управляющего регистра r7 каждого чипа.
-    uint8_t tic_ff = 0;                              // Считаем время воспроизведения (колличество прерываний).
+    mutable uint8_t     r7_reg[AY_NUMBER] = { 0 };   // Текущее состояние управляющего регистра r7 каждого чипа.
+    uint8_t tic_ff      = 0;                         // Считаем время воспроизведения (колличество прерываний).
 };
+
+#include "ay_ym_low_lavel_func.h"
--- ay_ym_low_lavel/ay_ym_low_lavel_func.h
+++ ay_ym_low_lavel/ay_ym_low_lavel_func.h
@@ -1,15 +1,19 @@
 #include "ay_ym_low_lavel.h"
 
+template < uint8_t AY_NUMBER, uint8_t TASK_PRIO, const ay_ym_low_lavel_cfg_t* const STRUCT_INIT >
+const ay_ym_low_lavel< AY_NUMBER, TASK_PRIO, STRUCT_INIT >* ay_ym_low_lavel< AY_NUMBER, TASK_PRIO, STRUCT_INIT >::instance ( void ) {
+    static const ay_ym_low_lavel< AY_NUMBER, TASK_PRIO, STRUCT_INIT >obj;
+    return &obj;
+}
 
 
-template < uint8_t AY_NUMBER, uint8_t QUEUE_LENGTH >
-void ay_ym_low_lavel< AY_NUMBER, QUEUE_LENGTH >::init ( void ) const {
-    this->queue         = USER_OS_STATIC_QUEUE_CREATE( QUEUE_LENGTH, sizeof( ay_queue_struct ), this->queue_buf, &this->q_st_buf );
+template < uint8_t AY_NUMBER, uint8_t TASK_PRIO, const ay_ym_low_lavel_cfg_t* const STRUCT_INIT >
+void ay_ym_low_lavel< AY_NUMBER, TASK_PRIO, STRUCT_INIT >::init ( void ) const {
     this->semaphore     = USER_OS_STATIC_BIN_SEMAPHORE_CREATE( &this->semaphore_buf );
 
     memset( this->r7_reg, 0b111111, AY_NUMBER );         // Все чипы отключены.
 
-    USER_OS_STATIC_TASK_CREATE( this->task, "ay_low", 300, ( void* )this, this->cfg->priority_task, this->task_stack, &this->task_struct );
+    USER_OS_STATIC_TASK_CREATE( this->task, "ay_low", 300, ( void* )this, TASK_PRIO, this->task_stack, &this->task_struct );
 }
 
 
@@ -19,8 +23,8 @@ void ay_ym_low_lavel< AY_NUMBER, QUEUE_LENGTH >::init ( void ) const {
  * Предполагается, что туда уже положили значения регистров для каждого AY/YM.
  * BDIR и BC1 дергаются так, чтобы произошел выбор регистра.
  */
-template < uint8_t AY_NUMBER, uint8_t QUEUE_LENGTH >
-void ay_ym_low_lavel< AY_NUMBER, QUEUE_LENGTH >::out_reg ( void ) const {
+template < uint8_t AY_NUMBER, uint8_t TASK_PRIO, const ay_ym_low_lavel_cfg_t* const STRUCT_INIT >
+void ay_ym_low_lavel< AY_NUMBER, TASK_PRIO, STRUCT_INIT >::out_reg ( void ) const {
     this->cfg->sr->write();
     this->cfg->bc1->set();
     this->cfg->bdir->set();
@@ -32,8 +36,8 @@ void ay_ym_low_lavel< AY_NUMBER, QUEUE_LENGTH >::out_reg ( void ) const {
  * Загружаем в заранее выбранный регистр значение.
  * Предполагается, что в буффер по адресу p_sr_data уже были загружены нужные значения.
  */
-template < uint8_t AY_NUMBER, uint8_t QUEUE_LENGTH >
-void ay_ym_low_lavel< AY_NUMBER, QUEUE_LENGTH >::out_data ( void ) const {
+template < uint8_t AY_NUMBER, uint8_t TASK_PRIO, const ay_ym_low_lavel_cfg_t* const STRUCT_INIT >
+void ay_ym_low_lavel< AY_NUMBER, TASK_PRIO, STRUCT_INIT >::out_data ( void ) const {
     this->cfg->sr->write();
     this->cfg->bdir->set();
     this->cfg->bdir->reset();
@@ -42,8 +46,8 @@ void ay_ym_low_lavel< AY_NUMBER, QUEUE_LENGTH >::out_data ( void ) const {
 /*
  * Вызывается в прерывании по переполнению таймера, настроенного на прерывание раз в 50 мс (по-умолчанию, значение может меняться).
  */
-template < uint8_t AY_NUMBER, uint8_t QUEUE_LENGTH >
-void ay_ym_low_lavel< AY_NUMBER, QUEUE_LENGTH >::timer_interrupt_handler ( void ) const {
+template < uint8_t AY_NUMBER, uint8_t TASK_PRIO, const ay_ym_low_lavel_cfg_t* const STRUCT_INIT >
+void ay_ym_low_lavel< AY_NUMBER, TASK_PRIO, STRUCT_INIT >::timer_interrupt_handler ( void ) const {
     static USER_OS_PRIO_TASK_WOKEN     prio;
     USER_OS_GIVE_BIN_SEMAPHORE_FROM_ISR( this->semaphore, &prio );    // Отдаем симафор и выходим (этим мы разблокируем поток, который выдает в чипы данные).
 }
@@ -53,8 +57,8 @@ void ay_ym_low_lavel< AY_NUMBER, QUEUE_LENGTH >::timer_interrupt_handler ( void 
  * Данные должны быть расположены в формате регистр(16-8 бит)|значение(7-0 бит).
  * Задача выполняется из под FreeRTOS.
  */
-template < uint8_t AY_NUMBER, uint8_t QUEUE_LENGTH >
-void ay_ym_low_lavel< AY_NUMBER, QUEUE_LENGTH >::queue_add_element ( ay_queue_struct* item ) const {
+template < uint8_t AY_NUMBER, uint8_t TASK_PRIO, const ay_ym_low_lavel_cfg_t* const STRUCT_INIT >
+void ay_ym_low_lavel< AY_NUMBER, TASK_PRIO, STRUCT_INIT >::queue_add_element ( ay_queue_struct* item ) const {
     ay_low_out_data buf;
     buf.reg     = item->reg;
     buf.data    = item->data;
@@ -63,8 +67,8 @@ void ay_ym_low_lavel< AY_NUMBER, QUEUE_LENGTH >::queue_add_element ( ay_queue_st
 }
 
 // Включить/выключить 1 канал одного из чипов. Через очередь.
-template < uint8_t AY_NUMBER, uint8_t QUEUE_LENGTH >
-void ay_ym_low_lavel< AY_NUMBER, QUEUE_LENGTH >::set_channel ( uint8_t number_ay, uint8_t channel, bool set ) const {
+template < uint8_t AY_NUMBER, uint8_t TASK_PRIO, const ay_ym_low_lavel_cfg_t* const STRUCT_INIT >
+void ay_ym_low_lavel< AY_NUMBER, TASK_PRIO, STRUCT_INIT >::set_channel ( uint8_t number_ay, uint8_t channel, bool set ) const {
     if ( set ) {                                                                // Если включить канал.
         this->r7_reg[number_ay] &= ~( 1 << channel);
     } else {
@@ -78,8 +82,8 @@ void ay_ym_low_lavel< AY_NUMBER, QUEUE_LENGTH >::set_channel ( uint8_t number_ay
 }
 
 // Чистим все AY/YM без использования очереди. Предполагается, что при этом никак не может произойти выдача из очереди.
-template < uint8_t AY_NUMBER, uint8_t QUEUE_LENGTH >
-void ay_ym_low_lavel< AY_NUMBER, QUEUE_LENGTH >::hardware_clear ( void ) const {
+template < uint8_t AY_NUMBER, uint8_t TASK_PRIO, const ay_ym_low_lavel_cfg_t* const STRUCT_INIT >
+void ay_ym_low_lavel< AY_NUMBER, TASK_PRIO, STRUCT_INIT >::hardware_clear ( void ) const {
     /*
      * В каждом AY необходимо в регистр R7 положить 0b111111 (чтобы остановить генерацию звука и шумов).
      * А во все остальные регистры 0.
@@ -87,18 +91,18 @@ void ay_ym_low_lavel< AY_NUMBER, QUEUE_LENGTH >::hardware_clear ( void ) const {
     for (int loop_ay = 0; loop_ay < AY_NUMBER; loop_ay++){
         this->r7_reg[loop_ay] = 0b111111;    // Сохраняем во внутреннее хранилище новое сосотояние всех чипов.
     };
-    memset(this->cfg->p_sr_data, 7, AY_NUMBER);        // Во всех чипах выбираем 7-й регистр (регистр управления).
+    memset(this->cfg->p_sr_data, 7, AY_NUMBER );        // Во всех чипах выбираем 7-й регистр (регистр управления).
     this->out_reg();
-    memset(this->cfg->p_sr_data, 0b111111, AY_NUMBER); // Во все 7-е регистры кладем команду отключения всех шумов и звуков на каналах.
-    this->out_reg();
+    memset(this->cfg->p_sr_data, 0b111111, AY_NUMBER ); // Во все 7-е регистры кладем команду отключения всех шумов и звуков на каналах.
+    this->out_data();
     for (int l = 0; l<7; l++) {            // 0..6 регистры заполняются 0-ми.
-        memset(this->cfg->p_sr_data, l, AY_NUMBER);
+        memset(this->cfg->p_sr_data, l, AY_NUMBER );
         this->out_reg();
-        memset(this->cfg->p_sr_data, 0, AY_NUMBER);
+        memset(this->cfg->p_sr_data, 0, AY_NUMBER );
         this->out_data();
     };
     for (int l = 8; l<0xF; l++){            // 8..15 регистры заполняются 0-ми.
-        memset(this->cfg->p_sr_data, l, AY_NUMBER);
+        memset(this->cfg->p_sr_data, l, AY_NUMBER );
         this->out_reg();
         memset(this->cfg->p_sr_data, 0, AY_NUMBER);
         this->out_data();
@@ -109,58 +113,77 @@ void ay_ym_low_lavel< AY_NUMBER, QUEUE_LENGTH >::hardware_clear ( void ) const {
  * Данный поток будет выдавать из очереди 50 раз
  * в секунду данные (данные разделяются 0xFF). Разблокируется из ay_timer_handler.
  */
-template < uint8_t AY_NUMBER, uint8_t QUEUE_LENGTH >
-void ay_ym_low_lavel< AY_NUMBER, QUEUE_LENGTH >::task () const {
+template < uint8_t AY_NUMBER, uint8_t TASK_PRIO, const ay_ym_low_lavel_cfg_t* const STRUCT_INIT >
+void ay_ym_low_lavel< AY_NUMBER, TASK_PRIO, STRUCT_INIT >::task ( void* p_this ) {
+        ay_ym_low_lavel* obj = ( ay_ym_low_lavel* ) p_this;
         ay_low_out_data buffer[AY_NUMBER];     // Буфер для адрес/команда для всех чипов.
-        uint32_t flag;                         // Внутренняя переменная "опустошения очереди". Она будет сравниваться с flag_over (читать подробное описание у этой переменной).
-        this->hardware_clear();                // Важно очистить чипы полностью без использования очереди.
+        volatile uint32_t flag;                         // Внутренняя переменная "опустошения очереди". Она будет сравниваться с flag_over (читать подробное описание у этой переменной).
+        obj->hardware_clear();                // Важно очистить чипы полностью без использования очереди.
+        // Костыль для теста!!!
+        ay_low_out_data buf;
+        buf.reg     = 7;
+        buf.data    = 0b11111000;
+        xQueueSend(obj->cfg->p_queue_array[0], &buf, portMAX_DELAY);
+
+        buf.reg     = 0;
+        buf.data    = 12;
+        xQueueSend(obj->cfg->p_queue_array[0], &buf, portMAX_DELAY);
+
+        buf.reg     = 1;
+        buf.data    = 2;
+        xQueueSend(obj->cfg->p_queue_array[0], &buf, portMAX_DELAY);
+
+        buf.reg     = 8;
+        buf.data    = 10;
+        xQueueSend(obj->cfg->p_queue_array[0], &buf, portMAX_DELAY);
+        xSemaphoreGive(obj->semaphore);
+
         while( true ) {
             flag = 0;            // Предположим, что данные есть во всех очерядях.
-            xSemaphoreTake ( this->semaphore, portMAX_DELAY );   // Как только произошло прерывание (была разблокировка из ay_timer_handler).
+            xSemaphoreTake ( obj->semaphore, portMAX_DELAY );   // Как только произошло прерывание (была разблокировка из ay_timer_handler).
                 while (flag != (0xFFFFFFFF >>(32-AY_NUMBER)) ) {    // Выдаем данные, пока все очереди не освободятся.]
                     /*
                      * Собираем из всех очередей пакет регистр/значение (если нет данных, то NO_DATA_FOR_AY).
                      */
-                    for ( int chip_loop = 0; chip_loop < AY_NUMBER; chip_loop++ ) {    // Собираем регистр/данные со всех очередей всех чипов.
-                        if ( ( flag & ( 1 << chip_loop ) ) != 0) {    // Если до этого в очереди конкретного чипа еще были элементы.
-                            if (uxQueueMessagesWaiting(this->queue_out[chip_loop]) != 0) {    // Если для этого чипа очередь не пуста.
-                                xQueueReceive(this->cfg->p_queue_array[chip_loop], &buffer[chip_loop], 0);    // Достаем этот эхлемент без ожидания, т.к. точно знаем, что он есть.
-                                if ( buffer.reg == 0xFF ){    // Если это флаг того, что далее читать можно лишь в следущем прерывании,...
+                    for ( volatile uint8_t chip_loop = 0; chip_loop < AY_NUMBER; chip_loop++ ) {    // Собираем регистр/данные со всех очередей всех чипов.
+                        volatile uint32_t count = uxQueueMessagesWaiting(obj->cfg->p_queue_array[chip_loop]);
+                        if ( count != 0) {    // Если для этого чипа очередь не пуста.
+                                xQueueReceive(obj->cfg->p_queue_array[chip_loop], &buffer[chip_loop], 0);    // Достаем этот эхлемент без ожидания, т.к. точно знаем, что он есть.
+                                if ( buffer[chip_loop].reg == 0xFF ){    // Если это флаг того, что далее читать можно лишь в следущем прерывании,...
                                     flag |= 1<<chip_loop; // то защищаем эту очередь от последущего считывания в этом прерывании.
                                 } else {    // Если пришли реальные данные.
-                                    if (  buffer.reg == 7){            // Сохраняем состояние 7-го регситра разрешения генерации звука и шумов. Чтобы в случае паузы было что вернуть. После затирания 0b111111 (отключить генерацию всего).
-                                        this->r7_reg[chip_loop] =  buffer.reg;
+                                    if (  buffer[chip_loop].reg == 7){            // Сохраняем состояние 7-го регситра разрешения генерации звука и шумов. Чтобы в случае паузы было что вернуть. После затирания 0b111111 (отключить генерацию всего).
+                                        obj->r7_reg[chip_loop] =  buffer[chip_loop].reg;
                                     }
                                 }
                             } else {
                                 flag |= 1<<chip_loop;        // Показываем, что в этой очереди закончились элементы.
                             }
-                        }
                     }
 
                     /*
                      * Собранный пакет раскладываем на регистры и на их значения и отправляем.
                      */
                     for (int chip_loop = 0; chip_loop < AY_NUMBER; chip_loop++){    // Раскладываем на регистры.
-                        this->cfg->p_sr_data[chip_loop] = buffer[chip_loop].reg;
+                        obj->cfg->p_sr_data[chip_loop] = buffer[chip_loop].reg;
                     }
-                    this->out_reg();
+                    obj->out_reg();
                     for (int chip_loop = 0; chip_loop < AY_NUMBER; chip_loop++){    // Раскладываем на значения.
-                        this->cfg->p_sr_data[chip_loop] = buffer[chip_loop].data;
+                        obj->cfg->p_sr_data[chip_loop] = buffer[chip_loop].data;
                     }
-                    this->out_data();
+                    obj->out_data();
                 }
 
             /*
              * В случае, если идет отслеживание секунд воспроизведения, то каждую секунду отдаем симафор.
              */
-            this->cfg->tic_ff++;
-            if (this->cfg->tic_ff == 50){        // Если насчитали секунду.
-                this->cfg->tic_ff = 0;
-                if (this->cfg->semaphore_sec_out != NULL){    // Если есть соединение семофором, то отдать его.
-                     xSemaphoreGive(*this->cfg->semaphore_sec_out);
+         /*   obj->cfg->tic_ff++;
+            if (obj->cfg->tic_ff == 50){        // Если насчитали секунду.
+                obj->cfg->tic_ff = 0;
+                if (obj->cfg->semaphore_sec_out != NULL){    // Если есть соединение семофором, то отдать его.
+                     xSemaphoreGive(*obj->cfg->semaphore_sec_out);
                 };
-            };
+            };*/
     }
 }
 
@@ -169,20 +192,20 @@ void ay_ym_low_lavel< AY_NUMBER, QUEUE_LENGTH >::task () const {
 void ay_play_stait (int fd, uint8_t stait){
     ay_init_t *d = eflib_getInstanceByFd (fd);
 
-    memset(this->cfg->p_sr_data, 7, AY_NUMBER);    // В любом случае писать будем в R7.
+    memset(this->cfg->p_sr_data, 7, AY_NUMBER );    // В любом случае писать будем в R7.
     _out_reg(d);
 
     if (stait){
         port_timer_set_stait(*this->cfg->tim_frequency_ay_fd, 1);
         port_timer_set_stait(*this->cfg->tim_event_ay_fd, 1);                    // Запускаем генерацию сигнала.
-        for (int loop_ay = 0; loop_ay<AY_NUMBER; loop_ay++){        // Возвращаем состояние всех AY.
+        for (int loop_ay = 0; loop_ay<AY_NUMBER, TASK_PRIO, STRUCT_INIT; loop_ay++){        // Возвращаем состояние всех AY.
             this->cfg->p_sr_data[loop_ay] = this->r7_reg[loop_ay];
         };
         _out_data(d);
     } else {
         port_timer_set_stait(*this->cfg->tim_event_ay_fd, 0);    // Останавливаем генерацию сигнала.
         port_timer_set_stait(*this->cfg->tim_frequency_ay_fd, 0);
-        memset(this->cfg->p_sr_data, 0b111111, AY_NUMBER);
+        memset(this->cfg->p_sr_data, 0b111111, AY_NUMBER );
         _out_data(d);
     };
 }
@@ -194,7 +217,7 @@ void ay_music_off (int fd){
     _ay_hardware_clear(d);    // Регистры в исходное состояние.
     // Очищаем очереди всех AY.
     uint16_t buf;
-    for (int loop_ay = 0; loop_ay<AY_NUMBER; loop_ay++){        // Извлекаем данные из всех очередей.
+    for (int loop_ay = 0; loop_ay<AY_NUMBER, TASK_PRIO, STRUCT_INIT; loop_ay++){        // Извлекаем данные из всех очередей.
         while ((uxQueueMessagesWaiting(this->queue_out[loop_ay]) != 0 )){    // Если очередь не пуста (Есть хотя бы 1 пакет)- извлекаем все.
             xQueueReceive(this->queue_out[loop_ay], &buf, 0);
         }
