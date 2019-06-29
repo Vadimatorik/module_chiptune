b7b6fb298c310ef9721880f9b6dcf7ec1c9da2a5
Vadim Deryabkin <Vadimatorikda@gmail.com>
2019-06-29 17:52:52 +0700

    mc_file_container будет использовать fatfs (заготовка).

--- aym/file_play/mc_file_container/inc/mc_file_container.h
+++ aym/file_play/mc_file_container/inc/mc_file_container.h
@@ -1,9 +1,14 @@
 #include "container.h"
 
+#include "fat.h"
+
 class mc_file_container : public Binary::Container {
 public:
     Ptr GetSubcontainer(std::size_t offset, std::size_t size) const;
     const void* Start() const;
     std::size_t Size() const;
+
+private:
+    fat f;
 
 };
